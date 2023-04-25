afd0d54537f13d95f62ba78aead2023f7bb1a852
merveakca <smkirac@gmail.com>
2023-04-26 00:45:07 +0300

    Add project files.

---
+++ Abc.Northwind.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.MvcWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abc.Northwind.MvcWebUI.Controllers
+{
+    public class ProductController:Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public ActionResult Index()
+        {
+            var products = _productService.GetAll();
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = products
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Abc.Northwind.MvcWebUI/Startup.cs
@@ -0,0 +1,40 @@
+using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Business.Concrete;
+using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace Abc.Northwind.MvcWebUI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<IProductDal, EfProductDal>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
