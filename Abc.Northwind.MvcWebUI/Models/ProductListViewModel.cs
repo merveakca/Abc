6538a3a333b2463dbee80a3ae38a66dd45a0d799
merveakca <smkirac@gmail.com>
2023-05-24 16:01:48 +0300

    new

--- Abc.Northwind.MvcWebUI/Models/ProductListViewModel.cs
+++ Abc.Northwind.MvcWebUI/Models/ProductListViewModel.cs
@@ -9,5 +9,9 @@ namespace Abc.Northwind.MvcWebUI.Models
     public class ProductListViewModel
     {
         public List<Product> Products { get; internal set; }
+        public int PageCount { get; internal set; }
+        public int PageSize { get; internal set; }
+        public int CurrentCategory { get; internal set; }
+        public int CurrentPage { get; internal set; }
     }
 }
