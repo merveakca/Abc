32188312aaaf68c485dee9e677e63928a29e16e2
merveakca <smkirac@gmail.com>
2023-05-31 15:43:18 +0300

    new

--- Abc.Northwind.MvcWebUI/Controllers/AdminController.cs
+++ Abc.Northwind.MvcWebUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.Concrete;
 using Abc.Northwind.MvcWebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,12 @@ namespace Abc.Northwind.MvcWebUI.Controllers
     public class AdminController:Controller
     {
         private IProductService _productService;
+        private ICategoryService _categoryService;
 
-        public AdminController(IProductService productService)
+        public AdminController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public ActionResult Index()
@@ -24,6 +27,28 @@ namespace Abc.Northwind.MvcWebUI.Controllers
                 Products = _productService.GetAll()
             };
             return View(productListViewModel);
+        }
+
+        public ActionResult Add()
+        {
+            var model = new ProductAddViewModel
+            {
+                Product = new Product(),
+                Categories = _categoryService.GetAll()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Add(product);
+                TempData.Add("message", "Product was successfully added");
+            }
+
+            return RedirectToAction("Add");
         }
     }
 }
